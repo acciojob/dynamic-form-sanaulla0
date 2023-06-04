2402c9aed5da429367809cc59e5d1770ce0bfa1b
sanaulla0 <vadapallisanaulla@gmail.com>
2023-06-04 22:22:08 +0530

    submitting code-Sun Jun 04 2023 22:22:07 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,49 @@
 
-import React from "react";
+import React ,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [flag , setFlag] = useState([]);
+
+const handelSet = (e) =>{
+           e.preventDefault();
+          setFlag([...flag,""]);
+         
+}
+const handlechange = (index,flag)=>{
+   const prev = [...flag];
+   prev[index] = value;
+   setFlag(prev);
+   
+}
+const handledelete = (index)=>{
+     let dummy = [...flag];
+     dummy.splice(index,1);
+     setFlag(dummy);
+}
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      
+         
+          {flag.length ===0 ? 
+         <p>No feild in the form</p>  
+         : (
+          flag.map((item,index)=>(
+        
+            <div>
+                    <input type="text" value={item} onChange={(e)=>handlechange(index,e.target.value)}  />  &nbsp;
+                    <button type="button"className="delete" value={item} onClick={()=>handledelete(index)}>delete</button>  <br/>
+     &nbsp;
+                 </div>
+     
+             ))
+         )
+        }
+        <br/>
+      
+ 
+        <button className="add"  onClick= {handelSet}>Add feild</button>
     </div>
   )
 }
